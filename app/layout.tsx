c01b46247f366d53d083c62b29a6cef2e98c555d
MachadONLY <rm551570@fiap.com.br>
2024-04-03 11:34:46 -0300

    login-icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className} bg-slate-950 text-white`}>{children}</body>
+      <body className={`${inter.className} h-screen bg-slate-950 text-white`}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,50 @@
 import Image from "next/image";
+import { Axe } from "lucide-react";
+import { Facebook } from 'lucide-react';
 
 export default function Home() {
   return (
     <>
-     
-        <div className="flex flex-col gap-2 p-6">
-          <h1 className="font-semibold text-3xl mt-10 ml-6">AXE 🪓</h1>
-          <h3 className="text-lg ml-6 mt-10">Login</h3>
-          <h1 className="text-2xl ml-6">Bem-vindo de volta</h1>
+      <div className="flex flex-col gap-2 p-6">
+        <h1 className="flex items-center gap-4 font-semibold text-3xl mt-10 ml-4">
+          AXE <Axe className="text-sky-500 w-8 h-8" />
+        </h1>
+        <h3 className="text-lg ml-4 mt-10">Login</h3>
+        <h1 className="text-2xl ml-4">Bem-vindo de volta</h1>
 
-          <label className="ml-6 mt-6" htmlFor="e-mail">E-mail <span className="text-cyan-300">*</span></label>
-          <input className="bg-white/10 ml-4 rounded-lg p-4" placeholder="Digite seu e-mail" type="e-mail" />
-          <label className="ml-6 mt-6" htmlFor="password">Password</label>
-          <input className="bg-white/10 ml-4 rounded-lg p-4" placeholder="Digite sua senha" type="password" />
-          <p className="flex justify-end mt-4">Esqueceu a senha?</p>
-          <button className="mt-10 bg-violet-800 rounded-lg p-4 ml-4 text-lg font-semibold">Login</button>
-          <div className="flex items-center gap-x-4 justify-center mt-4">
-            <div className="border w-20 h-0.5"></div>
-            <p className="text-lg">Ou</p>
-            <div className="border w-20 h-0.5"></div>
-          </div>
-
-          <div className="flex">
-            <div>
-              
-            </div>
-          </div>
+        <label className="ml-4 mt-6" htmlFor="e-mail">
+          E-mail <span className="text-cyan-300">*</span>
+        </label>
+        <input
+          className="bg-white/10 ml-4 rounded-lg p-4"
+          placeholder="Digite seu e-mail"
+          type="e-mail"
+        />
+        <label className="ml-4 mt-6" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="bg-white/10 ml-4 rounded-lg p-4"
+          placeholder="Digite sua senha"
+          type="password"
+        />
+        <p className="flex justify-end mt-4">Esqueceu a senha?</p>
+        <button className="mt-10 bg-gradient-to-r from-violet-800 to-blue-400 rounded-lg p-4 ml-4 text-lg font-semibold">
+          Login
+        </button>
+        <div className="flex items-center gap-x-4 justify-center mt-4">
+          <div className="border w-20 h-0.5"></div>
+          <p className="text-lg">Ou</p>
+          <div className="border w-20 h-0.5"></div>
         </div>
 
+        <div className="flex">
+          <div>
+            <Facebook />
+            Facebook
+          </div>
+        </div>
+      </div>
     </>
   );
 }
